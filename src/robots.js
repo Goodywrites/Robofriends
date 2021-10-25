4a2e007f61eaabfea78001356c9d9514051684ee
Goodywrites <ekpot7@gmail.com>
2021-10-25 14:22:44 -0700

    first commit

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+ {
+    id: 1,
+    name: 'Beni Abeni',
+    username: 'Beni',
+    email: 'abeni@gmail.com'
+ },
+ {
+    id: 2,
+    name: 'Temi Oke',
+    username: 'Tipsy',
+    email: 'tipsy@gmail.com'
+ },
+ {
+    id: 3,
+    name: 'Godswill Ekpot',
+    username: 'Getwiz',
+    email: 'getz@gmail.com'
+ },
+ {
+    id: 4,
+    name: 'Goodnews Tom',
+    username: 'Goody',
+    email: 'goody@gmail.com'
+ },
+ {
+    id: 5,
+    name: 'Sandra Tom',
+    username: 'Sandy',
+    email: 'sandy@gmail.com'
+ },
+ {
+    id: 6,
+    name: 'Joanne Dankai',
+    username: 'Joan',
+    email: 'joan@gmail.com'
+ },
+ {
+    id: 7,
+    name: 'Dami Oke',
+    username: 'Dami',
+    email: 'dami@gmail.com'
+ },
+];
